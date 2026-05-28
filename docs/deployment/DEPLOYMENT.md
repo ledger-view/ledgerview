a035e0c490e97ef172c4150d566298cd9284deab
mikolajdevs <mikolajdevs@gmail.com>
2026-05-28 22:13:22 +0200

    chore: configure deployment

---
+++ docs/deployment/DEPLOYMENT.md
@@ -0,0 +1,5 @@
+# Deployment
+
+1. `npm install -g gh-pages`
+2. `npm run build --base-href /ledgerview/`
+3. `npx gh-pages -d dist/ledgerview/browser`
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { environment } from '../environments/environment';
 
 export const authCodeFlowConfig: AuthConfig = {
   issuer: environment.keycloakIssuer,
-  redirectUri: window.location.origin,
+  redirectUri: window.location.origin + (window.location.pathname.startsWith('/ledgerview') ? '/ledgerview' : ''),
   clientId: 'ledgerview-web',
   responseType: 'code',
   scope: 'openid profile',
